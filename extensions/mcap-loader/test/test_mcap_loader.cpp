43b1159ea2c8b319ae8a59d46d3ad795a81e840a
Pablo Iñigo Blasco <pablo.inigo.blasco@gmail.com>
2026-03-16 13:04:51 +0100

    chore(mcap-loader): bump version to 1.0.9 (#13)
    Update version string in all 4 required locations:
    - manifest.json
    - PluginEntry.cpp (getMetadata)
    - McapLoader.cpp (getVersion)
    - test_mcap_loader.cpp (version assertion)

--- extensions/mcap-loader/test/test_mcap_loader.cpp
+++ extensions/mcap-loader/test/test_mcap_loader.cpp
@@ -4,6 +4,6 @@
 using namespace PluginSystem;
 
 TEST(McapLoaderTest, GetName)     { McapLoader p; EXPECT_EQ(p.getName(), "MCAP Loader"); }
-TEST(McapLoaderTest, GetVersion)  { McapLoader p; EXPECT_EQ(p.getVersion(), "1.0.0"); }
+TEST(McapLoaderTest, GetVersion)  { McapLoader p; EXPECT_EQ(p.getVersion(), "1.0.9"); }
 TEST(McapLoaderTest, OpenEmpty)   { McapLoader p; EXPECT_FALSE(p.open("")); }
 TEST(McapLoaderTest, OpenFile)    { McapLoader p; EXPECT_TRUE(p.open("log.mcap")); EXPECT_FALSE(p.getChannels().empty()); }
